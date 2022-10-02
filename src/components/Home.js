3693a9c0c60cee310ac591638739ae15ae2e934e
whitekitepredictive <price_k_r@yahoo.com>
2022-10-02 07:11:42 +0100

    Step 1-6
    Add input validation to ensure the value entered is an integer between 1 and 200

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,37 @@
 import React, { useState } from 'react';
-import { InputNumber } from 'antd';
+import { Button, InputNumber, notification } from 'antd';
 
 function Home() {
+    const [count, setCount] = useState(0);
     const [fizzBuzzCount, setFizzBuzzCount] = useState(0);
+
+    const updateFizzBuzzCount = () => {
+        if (validate())
+            setFizzBuzzCount(count);
+    }
+    const validate = () => {
+        if (count) return true;
+        notification.error({
+            message: 'Error',
+            description: 'You must enter a number between 1 and 200',
+            duration: 5,
+        });
+        return false;
+    }
 
     return (
         <React.Fragment>
             <div className='container'>
                 <InputNumber
-                    value={fizzBuzzCount}
-                    onChange={(e) => setFizzBuzzCount(e)} />
+                    min={0}
+                    max={200}
+                    value={count}
+                    onChange={(e) => setCount(e)} />
+                <Button
+                    type='primary'
+                    onClick={updateFizzBuzzCount}>
+                    Go
+                </Button>
 
                 {
                     Array.from({ length: fizzBuzzCount }, (_, i) => i + 1).map((number) => (
