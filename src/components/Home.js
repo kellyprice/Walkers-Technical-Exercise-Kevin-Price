90b9ed501db28c128ed5843828786e0d66db351e
whitekitepredictive <price_k_r@yahoo.com>
2022-10-02 08:19:16 +0100

    Step 2-11
    Add styling to improve the look and feel of the solution

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, InputNumber, notification } from 'antd';
+import { Button, Form, InputNumber, notification, PageHeader } from 'antd';
 
 function Home() {
     const [count, setCount] = useState(0);
@@ -29,28 +29,53 @@ function Home() {
 
     return (
         <React.Fragment>
+            <PageHeader
+                className="site-page-header"
+                title="Walkers Global"
+                subTitle="Technical Exercise"
+            />
             <div className='container'>
-                <InputNumber
-                    min={0}
-                    max={200}
-                    value={count}
-                    onChange={(e) => setCount(e)} />
-                <Button
-                    type='primary'
-                    onClick={updateFizzBuzzCount}>
-                    Go
-                </Button>
-                <div>
-                    <Button
-                        disabled={start + 20 > fizzBuzzCount}
-                        onClick={() => setStart(start + 20)}>
-                        Next 20
-                    </Button>
-                </div>
+                <Form layout='inline'>
+                    <Form.Item
+                        layout='inline'
+                        label="Enter a number between 1 and 200">
+                        <InputNumber
+                            min={0}
+                            max={200}
+                            value={count}
+                            onChange={(e) => setCount(e)} />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button
+                            type='primary'
+                            onClick={updateFizzBuzzCount}>
+                            Go
+                        </Button>
+                    </Form.Item>
+                </Form>
+
+                <Form layout='inline' hidden={!fizzBuzzCount}>
+                    <Form.Item>
+                        <Button
+                            className='m-t-15'
+                            disabled={start === 1}
+                            onClick={() => setStart(start - 20)}>
+                            Prev 20
+                        </Button>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button
+                            className='m-t-15'
+                            disabled={start + 20 > fizzBuzzCount}
+                            onClick={() => setStart(start + 20)}>
+                            Next 20
+                        </Button>
+                    </Form.Item>
+                </Form>
 
                 {
                     Array.from({ length: Math.min(20, fizzBuzzCount - start + 1) }, (_, i) => i + start).map((number) => (
-                        <div key={number}>
+                        <div key={number} className='list-item'>
                             {number}.
                             {number % 3 === 0 ? <span className='fizz'>{' walkers' + daySuffix}</span> : ''}
                             {number % 5 === 0 ? <span className='buzz'>{' assessment' + daySuffix}</span> : ''}
